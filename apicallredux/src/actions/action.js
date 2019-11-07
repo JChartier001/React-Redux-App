b1385bd3e79a39620824015afeb7792dad78d3f2
JChartier001 <JChartier001@gmail.com>
2019-11-07 15:14:46 -0600

    function fetchpics

--- apicallredux/src/actions/action.js
+++ apicallredux/src/actions/action.js
@@ -1,3 +1,18 @@
+import axios from "axios";
+
 export const FETCH_PICS_START = "FETCH_PICS_START"
 export const FETCH_PICS_SUCCESS = "FETCH_PICS_SUCCESS"
 export const FETCH_PICS_FAILURE = "FETCH_PICS_START"
+
+export function fetchPics(){
+    return dispatch => {
+        dispatch({type: FETCH_PICS_START});
+        axios.get('https://api.unsplash.com/photos/random/?client_id=ed8539051ed3c2c9b1ca260c8a8cff80c457896697b22ec9894392e464120a87')    .then(response => {
+            dispatch({ type: FETCH_PICS_SUCCESS, payload: response.full})
+        })
+        .catch(err => {
+            dispatch({type: FETCH_PICS_FAILURE, payload: err})
+        });
+        };        
+        }
+    
